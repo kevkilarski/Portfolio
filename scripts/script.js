15461f9c6673bec6d2b1e3d66e785d9984bd5cf6
kevkilarski <kskilarski@gmail.com>
2021-11-11 21:21:11 -0500

    Completed header in css

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,9 @@ app.threeBars = document.querySelector('#mobileHamContainer');
 app.topBar = document.querySelector('#mobileHamBar1');
 app.middleBar = document.querySelector('#mobileHamBar2');
 app.bottomBar = document.querySelector('#mobileHamBar3');
-app.nav = document.querySelector('#nav');
+app.nav = document.querySelector('#topNavLinksContainer');
 
-app.arrow = document.querySelector('#arrowScroll')
+app.arrow = document.querySelector('#arrow')
 app.aboutMe = document.querySelector('#aboutMe');
 
 // Declare init method
