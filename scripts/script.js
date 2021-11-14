11ca57b9eae7873abf16cd5b7cd564782650c30f
kevkilarski <kskilarski@gmail.com>
2021-11-14 17:13:57 -0500

    Improved ham menu

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,17 @@ app.threeBars = document.querySelector('#mobileHamContainer');
 app.topBar = document.querySelector('#mobileHamBar1');
 app.middleBar = document.querySelector('#mobileHamBar2');
 app.bottomBar = document.querySelector('#mobileHamBar3');
-app.nav = document.querySelector('#topNavLinksContainer');
+app.topNav = document.querySelector('#topNavLinksContainer');
 
-app.arrow = document.querySelector('#arrow')
 app.aboutMe = document.querySelector('#aboutMe');
+app.skills = document.querySelector('#skills');
+app.projects = document.querySelector('#projects');
+app.contact = document.querySelector('#contact');
 
 // Declare init method
 app.init = () => {
   app.hamburgerMenu();
-  app.arrowScroll();
+  app.scroll();
 };
 
 app.hamburgerMenu = () => {
@@ -22,16 +24,25 @@ app.hamburgerMenu = () => {
     app.topBar.classList.toggle('bar1Transform');
     app.middleBar.classList.toggle('bar2Transform');
     app.bottomBar.classList.toggle('bar3Transform');
-    app.nav.classList.toggle('navBarExpanded');
+    app.topNav.classList.toggle('navBarExpanded');
   })
 }
 
-app.arrowScroll = () => {
-  app.arrow.addEventListener('click', () => {
-    console.log("hello");
+app.scroll = function() {
+  document.querySelector(`#navAboutMe`).addEventListener('click', () => {
     app.aboutMe.scrollIntoView({behavior: "smooth"});
   })
+  document.querySelector(`#navSkills`).addEventListener('click', () => {
+    app.skills.scrollIntoView({behavior: "smooth"});
+  })
+  document.querySelector(`#navProjects`).addEventListener('click', () => {
+    app.projects.scrollIntoView({behavior: "smooth"});
+  })
+  document.querySelector(`#navContact`).addEventListener('click', () => {
+    app.contact.scrollIntoView({behavior: "smooth"});
+  })
 }
+
 
 
 
