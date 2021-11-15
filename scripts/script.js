14317c27eb34d7b3b0c8dffc042b8e5127aff60c
kevkilarski <kskilarski@gmail.com>
2021-11-15 18:01:28 -0500

    Added title animation, req refactor

--- scripts/script.js
+++ scripts/script.js
@@ -13,12 +13,6 @@ app.skills = document.querySelector('#skills');
 app.projects = document.querySelector('#projects');
 app.contact = document.querySelector('#contact');
 
-// Declare init method
-app.init = () => {
-  app.hamburgerMenu();
-  app.scroll();
-};
-
 // Where to put helper functions?
 app.navBarToggle = () => {
   app.topBar.classList.toggle('bar1Transform');
@@ -26,6 +20,13 @@ app.navBarToggle = () => {
   app.bottomBar.classList.toggle('bar3Transform');
   app.topNav.classList.toggle('navBarExpanded');
 }
+
+// Declare init method
+app.init = () => {
+  app.hamburgerMenu();
+  app.scroll();
+  app.cursor();
+};
 
 app.hamburgerMenu = () => {
   app.threeBars.addEventListener('click', () => {
@@ -60,6 +61,52 @@ app.scroll = function() {
 }
 
 
+app.cursor = () => {
+  // setTimeout(function() {
+  let counterBar = 4;
+  let counterText = 5;
+    setInterval(function() {
+      counterBar--;
+      console.log("counterBar = ", counterBar);
+      console.log("counterText = ", counterText);
+      if (counterBar === 0) {
+        console.log("Yo!");
+        document.querySelector('#titleWeb').classList.add('titleWeb2');
+        setTimeout(function() {
+          console.log("Hey!");
+            let test1 = `hsl(${Math.floor(Math.random() * (200 - 160 + 1) + 160)} 100% 50%)`;
+            let test2 = `hsl(${Math.floor(Math.random() * (200 - 160 + 1) + 160)} 100% 50%)`;
+            let test3 = `hsl(${Math.floor(Math.random() * (200 - 160 + 1) + 160)} 100% 50%)`;
+            let test4 = `hsl(${Math.floor(Math.random() * (200 - 160 + 1) + 160)} 100% 50%)`;
+            let test5 = `hsl(${Math.floor(Math.random() * (200 - 160 + 1) + 160)} 100% 50%)`;
+            document.querySelector('#titleWeb').classList.add('titleWeb3');
+            document.querySelector('#titleWeb').classList.remove('titleWebFade');
+            document.querySelector('#titleWeb').style.backgroundImage = `linear-gradient(40deg, ${test1}, ${test2}, ${test3}, ${test4}, ${test5})`;
+          counterBar = 4;
+        }, 1000);
+        setTimeout(function() {
+          document.querySelector('#titleWeb').classList.remove('titleWeb2');
+          document.querySelector('#titleWeb').classList.add('titleWebFade');
+
+          // document.querySelector('#titleWeb').classList.remove('titleWebFade');
+        }, 2000);
+      }
+      toWhite = ((counterBar * 25) / 2) + '%';
+      console.log(toWhite);
+    }, 1000);
+    // document.querySelector('#titleWeb').style.backgroundImage = `none`;
+    // setInterval(function() {
+    //   console.log("Hey!");
+    //   let color1 = Math.floor(Math.random()*16777215).toString(16);
+    //   let color2 = Math.floor(Math.random()*16777215).toString(16);
+    //   // document.querySelector('#titleWeb').innerHTML = "hey!!!";
+    //   document.querySelector('#titleWeb').classList.add('titleWeb3');
+    //   document.querySelector('#titleWeb').style.backgroundImage = `linear-gradient(40deg, #${color1}, #${color2})`;
+    // }, 3000);
+
+  // }, 200);
+}
+  
 
 
 // Call init method
