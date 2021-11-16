0c3ab6de8a67713d049867f2cb72fbced4b8c6b3
kevkilarski <kskilarski@gmail.com>
2021-11-15 19:34:59 -0500

    Refactored title, about me, and skills

--- scripts/script.js
+++ scripts/script.js
@@ -7,13 +7,12 @@ app.topBar = document.querySelector('#mobileHamBar1');
 app.middleBar = document.querySelector('#mobileHamBar2');
 app.bottomBar = document.querySelector('#mobileHamBar3');
 app.topNav = document.querySelector('#topNavLinksContainer');
-
 app.aboutMe = document.querySelector('#aboutMe');
 app.skills = document.querySelector('#skills');
 app.projects = document.querySelector('#projects');
 app.contact = document.querySelector('#contact');
+app.titleWebDev = document.querySelector('#titleWebDev');
 
-// Where to put helper functions?
 app.navBarToggle = () => {
   app.topBar.classList.toggle('bar1Transform');
   app.middleBar.classList.toggle('bar2Transform');
@@ -24,8 +23,8 @@ app.navBarToggle = () => {
 // Declare init method
 app.init = () => {
   app.hamburgerMenu();
-  app.scroll();
-  app.cursor();
+  app.navScroll();
+  app.titleColor();
 };
 
 app.hamburgerMenu = () => {
@@ -34,7 +33,7 @@ app.hamburgerMenu = () => {
   })
 }
 
-app.scroll = function() {
+app.navScroll = function() {
   document.querySelector(`#arrow`).addEventListener('click', () => {
     app.aboutMe.scrollIntoView({behavior: "smooth"});
   })
@@ -61,52 +60,39 @@ app.scroll = function() {
 }
 
 
-app.cursor = () => {
-  // setTimeout(function() {
-  let counterBar = 4;
-  let counterText = 5;
-    setInterval(function() {
-      counterBar--;
-      console.log("counterBar = ", counterBar);
-      console.log("counterText = ", counterText);
-      if (counterBar === 0) {
-        console.log("Yo!");
-        document.querySelector('#titleWeb').classList.add('titleWeb2');
-        setTimeout(function() {
-          console.log("Hey!");
-            let test1 = `hsl(${Math.floor(Math.random() * (200 - 160 + 1) + 160)} 100% 50%)`;
-            let test2 = `hsl(${Math.floor(Math.random() * (200 - 160 + 1) + 160)} 100% 50%)`;
-            let test3 = `hsl(${Math.floor(Math.random() * (200 - 160 + 1) + 160)} 100% 50%)`;
-            let test4 = `hsl(${Math.floor(Math.random() * (200 - 160 + 1) + 160)} 100% 50%)`;
-            let test5 = `hsl(${Math.floor(Math.random() * (200 - 160 + 1) + 160)} 100% 50%)`;
-            document.querySelector('#titleWeb').classList.add('titleWeb3');
-            document.querySelector('#titleWeb').classList.remove('titleWebFade');
-            document.querySelector('#titleWeb').style.backgroundImage = `linear-gradient(40deg, ${test1}, ${test2}, ${test3}, ${test4}, ${test5})`;
-          counterBar = 4;
-        }, 1000);
-        setTimeout(function() {
-          document.querySelector('#titleWeb').classList.remove('titleWeb2');
-          document.querySelector('#titleWeb').classList.add('titleWebFade');
+app.titleColor = () => {
+  // Declare and initialize countdown for white bar rendering
+  let countdownForBar = 4;
 
-          // document.querySelector('#titleWeb').classList.remove('titleWebFade');
-        }, 2000);
-      }
-      toWhite = ((counterBar * 25) / 2) + '%';
-      console.log(toWhite);
-    }, 1000);
-    // document.querySelector('#titleWeb').style.backgroundImage = `none`;
-    // setInterval(function() {
-    //   console.log("Hey!");
-    //   let color1 = Math.floor(Math.random()*16777215).toString(16);
-    //   let color2 = Math.floor(Math.random()*16777215).toString(16);
-    //   // document.querySelector('#titleWeb').innerHTML = "hey!!!";
-    //   document.querySelector('#titleWeb').classList.add('titleWeb3');
-    //   document.querySelector('#titleWeb').style.backgroundImage = `linear-gradient(40deg, #${color1}, #${color2})`;
-    // }, 3000);
+  // Randomizes usable color options in hsl format
+  colorRandomizer = () => {
+    return `hsl(${Math.floor(Math.random() * (200 - 160 + 1) + 160)} 100% 50%)`;
+  }
 
-  // }, 200);
+  // One-second timer to structure title animation
+  setInterval(function() {
+    countdownForBar--;
+    if (countdownForBar === 0) {
+      // Begins bar animation, taking one second to fill text
+      app.titleWebDev.classList.add('titleBar');
+
+      // After one second, begin changing color of text
+      setTimeout(function() {
+        app.titleWebDev.classList.add('titleColorNew');
+        app.titleWebDev.classList.remove('titleColorToWhite');
+        app.titleWebDev.style.backgroundImage = `linear-gradient(40deg, ${colorRandomizer()}, ${colorRandomizer()}, ${colorRandomizer()}, ${colorRandomizer()}, ${colorRandomizer()}, ${colorRandomizer()})`;
+        countdownForBar = 4;
+      }, 1000);
+
+      // Bar animation ends, reset bar and begin color transition back to white
+      setTimeout(function() {
+        app.titleWebDev.classList.remove('titleBar');
+        app.titleWebDev.classList.add('titleColorToWhite');
+      }, 2000);
+    }
+  }, 1000);
 }
-  
+
 
 
 // Call init method
